714ed1bd45078ea3f5907266f6cc758ac0661961
Timandes White <timands@gmail.com>
2018-11-16 22:39:46 +0800

    feat(logger): add placeholder support

--- src/Log/DefaultLogger.php
+++ src/Log/DefaultLogger.php
@@ -15,8 +15,24 @@ class DefaultLogger extends AbstractLogger
     
     public function log($level, $message, array $context = array())
     {
+        $message = $this->interpolate($message, $context);
         fprintf(STDOUT, "%s %s %d %s %s" . PHP_EOL
                 , date('Y-m-d H:i:s'), $level, getmypid()
                 , $this->class, $message);
+    }
+
+    /**
+     * Interpolates context values into the message placeholders.
+     */
+    function interpolate($message, array $context = array())
+    {
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            $replace['{' . $key . '}'] = $val;
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
     }
 }
